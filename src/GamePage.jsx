e0a1359e97bd12f3b450d9364d02961aeb1bde06
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2022-05-22 21:44:17 +0700

    tes2 jam + game page tapi gagal

---
+++ src/GamePage.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import Button from "./components/button";
+import PopUpButton from "./components/popUpButton";
+import Profile from "./assets/popUpTeam.png";
+import QMark from "./assets/popUpQMark.png";
+import './App.css';
+
+function GamePage() {
+
+    return (
+    <>
+        <h1>Test</h1>
+    </>
+    );
+}
+
+export default GamePage;

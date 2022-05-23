33472bbde66434086ab88ac205043eff641b0ae0
bellashrns <bella.saharani@student.umn.ac.id>
2022-05-24 05:09:09 +0700

    dropdown 2

--- src/components/InputMajor.jsx
+++ src/components/InputMajor.jsx
@@ -6,16 +6,20 @@ export default function InputMajor() {
     const [option, setOption] = useState("");
 
     return (
-        <div id="inputMajor" align="end" className="row mx-auto">
-            <Dropdown onSelect={(e) => setOption(e)}>
-                <Dropdown.Toggle id="inputMajorBtn">
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {Majors.map((major) => {
-                        return <Dropdown.Item eventKey={major.value}>{major.value}</Dropdown.Item>
-                    })}
-                </Dropdown.Menu>
-            </Dropdown>
+        <div id="inputMajor" className="row mx-auto">
+            {/* <p>{option}</p> */}
+            <div align="end">
+                <Dropdown onSelect={(e) => setOption(e)}>
+                    <Dropdown.Toggle id="inputMajorBtn">
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {Majors.map((major) => {
+                            return <Dropdown.Item eventKey={major.value}>{major.value}</Dropdown.Item>
+                        })}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
         </div>
     );
 }

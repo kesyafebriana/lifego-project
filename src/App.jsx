4cfca2c887a174337f123e72ce7d5b5d2b021b32
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2022-05-23 19:50:18 +0700

    Masih berantakan tp bismillah aja deh

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
 import Button from "./components/button";
 import PopUpButton from "./components/popUpButton";
 import Profile from "./assets/popUpTeam.png";
 import QMark from "./assets/popUpQMark.png";
+import Logo from "./assets/logofix.png";
 import GameGuideBackground from "./assets/gameGuide.png";
 import CloseButton from "./assets/tombol-close.png";
 import GameGuide from './components/GameGuide';
@@ -22,15 +25,29 @@ function App() {
         setIsOpenAboutUs(!isOpenAboutUs);
     } 
 
+    const toGame = () => {
+        ReactDOM.render(<Game />, document.getElementById('root'));
+    } 
+
     return (
         <>
-            <Button item="START"/>
-
-            <PopUpButton pict={Profile} handleClick = {togglePopupAboutUs} />
-            {isOpenAboutUs && <AboutUs closeButton={CloseButton} pict={AboutUsBackground} handleClose={togglePopupAboutUs}/>}
-            
-            <PopUpButton pict={QMark} handleClick = {togglePopup} />
-            {isOpen && <GameGuide closeButton={CloseButton} pict={GameGuideBackground} handleClose={togglePopup}/>}
+            <div className="gameGuideButton">
+                <PopUpButton pict={QMark} handleClick = {togglePopup} />
+                {isOpen && <GameGuide closeButton={CloseButton} pict={GameGuideBackground} handleClose={togglePopup}/>}
+            </div>
+            <div className="aboutUsButton">
+                <PopUpButton pict={Profile} handleClick = {togglePopupAboutUs} />
+                {isOpenAboutUs && <AboutUs closeButton={CloseButton} pict={AboutUsBackground} handleClose={togglePopupAboutUs}/>}
+            </div>
+            <div className="logo">
+                <img width="27%" src={Logo} />
+            </div>
+            <div className="logoStart">
+                <Button 
+                    item="START"
+                    click={toGame}
+                />
+            </div>
         </>
     );
 }

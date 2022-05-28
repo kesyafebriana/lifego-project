6451499395ee66d1dfc6c309c28ce4bb230d29c7
bellashrns <bella.saharani@student.umn.ac.id>
2022-05-29 03:08:19 +0700

    bar + button

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import Time from "./components/Time";
+import Bar from "./components/Bar";
 import './Game.css';
 
 function Game() {
+    const [eat, setEat] = useState(0);
+    const [study, setStudy] = useState(0);
+    const [sleep, setSleep] = useState(0);
+    const [play, setPlay] = useState(0);
+
+    function handleEat() {
+        setEat(eat + 20);
+    }
+
+    function handleStudy() {
+        setStudy(study + 20);
+    }
+
+    function handleSleep() {
+        setSleep(sleep + 20);
+    }
+
+    function handlePlay() {
+        setPlay(play + 20);
+    }
 
     return (
         <>
             <Time />       
+            <Bar action={study} name="Lawsen" onClick={handleStudy}/>
         </>
     );
 }

3983d7afe1a4dbd4a436356c0ad3f64d3cef4c0f
bellashrns <bella.saharani@student.umn.ac.id>
2022-06-03 13:46:28 +0700

    ganti2 char

--- src/components/Images.js
+++ src/components/Images.js
@@ -2,10 +2,51 @@ import diegoNormal from './characters/diegoNormal.png';
 import doraNormal from './characters/doraNormal.png';
 import harryNormal from './characters/harryNormal.png';
 import lisaNormal from './characters/lisaNormal.png';
+
 import makan from '../assets/makan.png'
 import main from '../assets/main.png'
 import belajar from '../assets/belajar.png'
 import tidur from '../assets/tidur.png'
+
+import diegoMakan from './characters/diegomakanfix.png';
+import doraMakan from './characters/doramakanfix.png';
+import harryMakan from './characters/harrymakanfix.png';
+import lisaMakan from './characters/lisamakanfix.png';
+
+import diegoGame from './characters/diegogamefix.png';
+import doraGame from './characters/doragamefix.png';
+import harryGame from './characters/harrygamefix.png';
+import lisaGame from './characters/lisagamefix.png';
+
+import diegoBelajar from './characters/diegobelajarfix.png';
+import doraBelajar from './characters/dorabelajarfix.png';
+import harryBelajar from './characters/harrybelajarfix.png';
+import lisaBelajar from './characters/lisabelajarfix.png';
+
+import diegoTidur from './characters/diegotidurfix.png';
+import doraTidur from './characters/doratidurfix.png';
+import harryTidur from './characters/harrytidurfix.png';
+import lisaTidur from './characters/lisatidurfix.png';
+
+import diegoMinum from './characters/diegominumfix.png';
+import doraMinum from './characters/doraminumfix.png';
+import harryMinum from './characters/harryminumfix.png';
+import lisaMinum from './characters/lisaminumfix.png';
+
+import diegoMasak from './characters/diegomasakfix.png';
+import doraMasak from './characters/doramasakfix.png';
+import harryMasak from './characters/harrymasakfix.png';
+import lisaMasak from './characters/lisamasakfix.png';
+
+import diegoKuliah from './characters/diegokuliah.png';
+import doraKuliah from './characters/dorakuliah.png';
+import harryKuliah from './characters/harrykuliah.png';
+import lisaKuliah from './characters/lisakuliah.png';
+
+import diegoNgobrol from './characters/diegongobrolfix.png';
+import doraNgobrol from './characters/dorangobrolfix.png';
+import harryNgobrol from './characters/harryngobrolfix.png';
+import lisaNgobrol from './characters/lisangobrolfix.png';
 
 const Images = [
     {normal: [
@@ -19,6 +60,54 @@ const Images = [
         {url: main},
         {url: belajar},
         {url: tidur}
+    ]}, 
+    {makan: [
+        {url: diegoMakan},
+        {url: doraMakan},
+        {url: harryMakan},
+        {url: lisaMakan}
+    ]},
+    {game: [
+        {url: diegoGame},
+        {url: doraGame},
+        {url: harryGame},
+        {url: lisaGame}
+    ]},
+    {belajar: [
+        {url: diegoBelajar},
+        {url: doraBelajar},
+        {url: harryBelajar},
+        {url: lisaBelajar}
+    ]},
+    {tidur: [
+        {url: diegoTidur},
+        {url: doraTidur},
+        {url: harryTidur},
+        {url: lisaTidur}
+    ]},
+    {minum: [
+        {url: diegoMinum},
+        {url: doraMinum},
+        {url: harryMinum},
+        {url: lisaMinum}
+    ]},
+    {masak: [
+        {url: diegoMasak},
+        {url: doraMasak},
+        {url: harryMasak},
+        {url: lisaMasak}
+    ]},
+    {kuliah: [
+        {url: diegoKuliah},
+        {url: doraKuliah},
+        {url: harryKuliah},
+        {url: lisaKuliah}
+    ]},
+    {ngobrol: [
+        {url: diegoNgobrol},
+        {url: doraNgobrol},
+        {url: harryNgobrol},
+        {url: lisaNgobrol}
     ]}
 ];
 
